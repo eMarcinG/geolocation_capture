246f6e790195edab8018113a39b263d7bb9fd6c7
Marcin G <m.gumny@giap.pl>
2025-02-23 12:32:48 +0100

    rebuild endpoint for fetch and get data

---
+++ geolocation_capture/management/mixins.py
@@ -0,0 +1,53 @@
+from django.db import OperationalError
+from .models import Geolocation
+import socket
+
+class GeolocationDatabaseMixin:
+    def get_geolocation(self, **kwargs):
+        ip = kwargs.get('ip')
+        url = kwargs.get('url')
+
+        geolocations = []
+
+        if ip:
+            geolocations = self._get_geolocations_by_ip(ip)
+            if geolocations:
+                return self._select_best_geolocation(geolocations)
+
+        if url:
+            geolocations = self._get_geolocations_by_url(url)
+            if geolocations:
+                return self._select_best_geolocation(geolocations)
+
+        return None
+
+    def _get_geolocations_by_ip(self, ip):
+        try:
+            print(list(Geolocation.objects.using('default').filter(ip_address=ip)))
+            return list(Geolocation.objects.using('default').filter(ip_address=ip))
+            
+        except OperationalError:
+            try:
+                return list(Geolocation.objects.using('secondary').filter(ip_address=ip))
+            except OperationalError:
+                return []
+
+    def _get_geolocations_by_url(self, url):
+        try:
+            hostname = self._get_hostname_from_url(url)
+            ip = socket.gethostbyname(hostname)
+            return self._get_geolocations_by_ip(ip)
+        except socket.error:
+            try:
+                return list(Geolocation.objects.using('default').filter(url=url))
+            except OperationalError:
+                try:
+                    return list(Geolocation.objects.using('secondary').filter(url=url))
+                except OperationalError:
+                    return []
+
+    def _get_hostname_from_url(self, url):
+        return url.replace("http://", "").replace("https://", "").split('/')[0]
+
+    def _select_best_geolocation(self, geolocations):
+        return max(geolocations, key=lambda geo: geo.created_at)
--- geolocation_capture/management/viewsets.py
+++ geolocation_capture/management/viewsets.py
@@ -1,20 +1,32 @@
-from rest_framework import viewsets
+# Django imports
+from django.shortcuts import get_object_or_404
+
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
 from rest_framework.decorators import action
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from .models import Geolocation
 from .serializers import GeolocationSerializer
 from .tasks import fetch_and_store_geolocation
+from .mixins import GeolocationDatabaseMixin
+from .permissions import IsAdminOrReadOnly
 
-class GeolocationViewSet(viewsets.ModelViewSet):
+
+
+class GeolocationViewSet(viewsets.ModelViewSet, GeolocationDatabaseMixin):
     """
     A viewset for viewing and editing geolocation instances.
     """
     queryset = Geolocation.objects.all()
     serializer_class = GeolocationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['ip_address', 'url']
+    search_fields = ['ip_address', 'url']
 
-    @action(detail=False, methods=['post'])
+    @action(detail=False, methods=['post'], url_path='fetch', permission_classes=[IsAuthenticated])
     def fetch_geolocation(self, request):
         """
         A custom action to fetch geolocation data for a given IP address and store it.
@@ -26,3 +38,22 @@ class GeolocationViewSet(viewsets.ModelViewSet):
         result = fetch_and_store_geolocation.delay(ip)
 
         return Response({"status": "Task started", "task_id": result.id})
+
+    @action(detail=False, methods=['get'], url_path='search', permission_classes=[IsAuthenticated])
+    def search_geolocation(self, request):
+        """
+        A custom action to search geolocation data for a given IP address or URL.
+        """
+        ip = request.query_params.get('ip')
+        url = request.query_params.get('url')
+
+        if not ip and not url:
+            return Response({"error": "IP address or URL is required"}, status=400)
+
+        geolocation = self.get_geolocation(ip=ip, url=url)
+
+        if not geolocation:
+            return Response({"error": "No data found for the given IP address or URL"}, status=404)
+
+        serializer = GeolocationSerializer(geolocation)
+        return Response(serializer.data)
